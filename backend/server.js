78fb6b582447f6cde80c4e1e2fb211c19f6e1122
Nilushi Silshara <nilushi.weraharaarachchige@tuni.fi>
2025-02-19 10:09:46 +0200

    Integrate map component with backend api

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
+const cors = require('cors')
 const routeRoutes = require('./routes/routes.js');
 require('dotenv').config();
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
 // Routes
 app.use('/', routeRoutes);
 
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
