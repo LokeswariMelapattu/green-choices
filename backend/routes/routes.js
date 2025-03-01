5f944a8698adee76a97589f3975b9809c7d47afd
Rohanjai <jairohan2912@gmail.com>
2025-03-01 19:53:23 +0200

    Updated Routes for User endpoint

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { findRoutes } = require('../controllers/routeController');
+const userController = require('../controllers/userController');
 
 router.get('/find-routes', findRoutes);
+
+// Routes follow RESTful convention with base path /user
+router.post('/user', userController.createUser);
+router.put('/user/:id', userController.updateUser);
+router.get('/user/:id', userController.getUserById);
+router.get('/user', userController.getAllUsers);
+router.get('/user/:id/history', userController.getUserAuditHistory);
 
 module.exports = router;
