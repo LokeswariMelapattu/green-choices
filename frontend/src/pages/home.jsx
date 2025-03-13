997eeceee3d91899a16ce3296e7a0732f8f20750
winonakfernandes <winona.fernandes@tuni.fi>
2025-03-14 00:17:58 +0200

    products section added

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -24,12 +24,38 @@ const Home = () => {
       <Header />
       <main className="flex min-h-screen bg-gray-100">
         <div className="container mx-auto px-4 py-8">
-          <div className="flex flex-col justify-center gap-2 
+          <div className="flex flex-col justify-center gap-4 
           bg-gradient-to-br from-[#1FAE5B] via-[#1DA154] to-[#0D4826]
-          text-white w-full h-[350px] p-6 md:p-10
+          text-white w-full h-[330px] p-6 md:p-10
           rounded-lg shadow-md">
             <h1 className="text-5xl md:text-6xl font-bold font-quantico">FLASH SALE!!</h1>
             <FaGifts className="text-7xl md:text-8xl" />
+          </div>
+
+          <div className="py-6">
+            <h2 className="py-4 text-2xl font-semibold">Trending Now</h2>
+            <div className="flex gap-8 overflow-x-auto no-scrollbar">
+              {products.map(product => (
+                <div key={product.product_id}>
+                  <img src={product.img} alt={product.name} className="min-h-[200px] min-w-[200px] rounded-xl" />
+                  <h3 className="py-2">{product.name}</h3>
+                  <p>${product.price}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div>
+            <h2 className="py-4 text-2xl font-semibold">Best Deals</h2>
+            <div className="flex gap-8 overflow-x-auto no-scrollbar">
+              {products.map(product => (
+                <div key={product.product_id}>
+                  <img src={product.img} alt={product.name} className="min-h-[200px] min-w-[200px] rounded-xl" />
+                  <h3 className="py-2">{product.name}</h3>
+                  <p>${product.price}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </main>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,18 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* IE & Edge */
+          'scrollbar-width': 'none',    /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none',             /* Chrome, Safari */
+        },
+      })
+    }
+  ],
 }
 
