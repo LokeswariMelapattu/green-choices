4a5dc325b48c60b5b5b057e4f4e7354b3d8337c0
winonakfernandes <winona.fernandes@tuni.fi>
2025-04-11 14:58:55 +0300

    TC-004 test case implemented

--- frontend/src/pages/Checkout/_tests_/RouteSelector.test.jsx
+++ frontend/src/pages/Checkout/_tests_/RouteSelector.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import RouteSelector from "@/components/RouteSelector";
@@ -14,14 +14,25 @@ describe("RouteSelector Component", () => {
         cost: { minimum: 100, maximum: 200 },
         carbonEmissions: { minimum: 50, maximum: 80 }
       }
+    },
+    {
+      routeNumber: "A2",
+      segments: [{ transportModes: ["road"] }],
+      metrics: {
+        duration: { minimum: 2, maximum: 21 },
+        distance: { minimum: 7000 },
+        cost: { minimum: 197, maximum: 417 },
+        carbonEmissions: { minimum: 75, maximum: 190 }
+      }
     }
   ];
 
+
   it("should select the first delivery option by default", () => {
-    render(<RouteSelector routes={mockRoutes} selectedRoute={mockRoutes[0]} onRouteSelect={() => {}} />);
+    render(<RouteSelector routes={mockRoutes} selectedRoute={mockRoutes[0]} onRouteSelect={() => { }} />);
 
     // Find the route name inside the card
-    const routeText = screen.getByText("Route A1");
+    const routeText = screen.getByText("Option 1");
 
     // Traversing up until we find the div with a class "border-green-500 bg-green-50" used for the selected route
     let routeCard = routeText;
@@ -31,4 +42,41 @@ describe("RouteSelector Component", () => {
 
     expect(routeCard).toHaveClass("border-green-500 bg-green-50");
   });
+
+  it("should highlight the new selected delivery option", () => {
+    const { rerender } = render(
+      <RouteSelector
+        routes={mockRoutes}
+        selectedRoute={mockRoutes[0]}
+        onRouteSelect={() => { }}
+      />
+    );
+
+    const option2Text = screen.getByText("Option 2");
+
+    let routeCard = option2Text;
+    while (routeCard && !routeCard.classList?.contains("min-w-[300px]")) {
+      routeCard = routeCard.parentElement;
+    }
+
+    expect(routeCard).not.toHaveClass("border-green-500", "bg-green-50");
+
+    fireEvent.click(routeCard);
+
+    rerender(
+      <RouteSelector
+        routes={mockRoutes}
+        selectedRoute={mockRoutes[1]}
+        onRouteSelect={() => { }}
+      />
+    );
+
+    const updatedOption2 = screen.getByText("Option 2");
+    let updatedCard = updatedOption2;
+    while (updatedCard && !updatedCard.classList?.contains("min-w-[300px]")) {
+      updatedCard = updatedCard.parentElement;
+    }
+
+    expect(updatedCard).toHaveClass("border-green-500", "bg-green-50");
+  })
 });
