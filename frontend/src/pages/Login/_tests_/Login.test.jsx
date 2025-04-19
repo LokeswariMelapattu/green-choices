c7cd1c14cbdbd3cc21477753c69273f22e18d234
Tatu Kankare <tatu.kankare@tuni.fi>
2025-04-19 18:14:36 +0300

    Implemented TC-014

--- frontend/src/pages/Login/_tests_/Login.test.jsx
+++ frontend/src/pages/Login/_tests_/Login.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, within, fireEvent, cleanup } from "@testing-library/react";
+import { render, screen, waitFor, within, fireEvent, cleanup, act } from "@testing-library/react";
 import { describe, it, expect, vi, beforeEach} from "vitest";
 import "@testing-library/jest-dom/vitest";
 import App from "../../../App.jsx";
@@ -12,12 +12,41 @@ const testCreds = {
     falsePassw: 'incorrect'
 }
 
-// TODO make mock for TC-014
+// Mock for TC-014
+vi.mock('@/hooks/useUser', () => ({
+    default: () => ({
+        user: null,
+        loading: false,
+        error: null,
+        checkCredentials: vi.fn(async (e, p) => {
+            if (e == 'john.doe@example.com' && p == 'password123') {
+                return {"success":true,
+                    "data":{
+                        "userid":1,
+                        "firstname":"John",
+                        "lastname":"Doe",
+                        "address":"1234 Elm Street",
+                        "email":"john.doe@example.com",
+                        "createdat":"2025-04-17T12:44:21.747Z",
+                        "updatedat":"2025-04-17T12:44:21.747Z",
+                        "username":"John Doe"
+                    }
+                }
+            }
+            else {
+                return {"success":false,"error":"User not found"}
+            }
+        })
+    }),
+    
+}));
 
 describe('Loginpage', () => {
     
     beforeEach(async () => {
-        cleanup();       
+        const useUser = (await import('@/hooks/useUser')).default();
+        cleanup();
+        useUser.checkCredentials.mockClear();       
         window.history.pushState({}, "", "/login");
     });
     
@@ -45,18 +74,22 @@ describe('Loginpage', () => {
     
     //TC-014
     it("Should redirect to /home when correct credentials are entered", async () => {
-
+        //const checkCredentials = vi.spyOn( (await import('@/hooks/useUser')).default(), 'checkCredentials');        
+        
         render(<App/>);
 
+        let usernamePrompt = await screen.getByPlaceholderText('Email', {selector: 'input'});
+        let passwordPrompt = await screen.getByPlaceholderText('Password', {selector: 'input'});
+        await act(async () => {
+            await fireEvent.change(usernamePrompt, {target:{value: testCreds.trueEmail}});
+            await fireEvent.change(passwordPrompt, {target:{value: testCreds.truePassw}});
+            //usernamePrompt.value = testCreds.trueEmail;
+            //passwordPrompt.value = testCreds.truePassw;
+            expect(await fireEvent.click(screen.getByRole('button', {name: /Login/}))).toBeTruthy();
+        });
         await waitFor(() => {
-            let usernamePrompt = screen.getByPlaceholderText('Email', {selector: 'input'});
-            let passwordPrompt = screen.getByPlaceholderText('Password', {selector: 'input'});
-            usernamePrompt.value = testCreds.trueEmail;
-            passwordPrompt.value = testCreds.truePassw;
-            console.log(window.location.href);
-            expect(fireEvent.click(screen.getByRole('button', {name: /Login/}))).toBeTruthy();
+            //expect(checkCredentials).toHaveBeenCalledWith(testCreds.trueEmail, testCreds.truePassw);
             expect(window.location.href).toContain('/home');
-            console.log(window.location.href);
         });
     });
     
